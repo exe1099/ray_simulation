1a6c7abbbb3cf0b7dec2a93d41e67ae535d4fdfe
exe1099 <exe1099@exe1099.com>
2018-05-16 14:07:02 +0200

    Fixed minor errors

--- fit_simulation_to_data.py
+++ fit_simulation_to_data.py
@@ -28,10 +28,12 @@ def residual(params, xdata, data_to_fit):
 
 # parameters
 params = fit.Parameters()
-params.add('n1', value=1.5, min=1.3, max=1.6, brute_step=0.02)
+params.add('n1', value=1.5, min=1.48, max=1.5, brute_step=0.02)
 params.add('scaling', value=1, min=0.9, max=1.1, brute_step=0.1, vary=False)
-params.add('repeat_sim', value=20, vary=False)
-params.add('n_rays', value=8*10**7, vary=False)
+#  params.add('repeat_sim', value=20, vary=False)
+params.add('repeat_sim', value=1, vary=False)
+#  params.add('n_rays', value=8*10**7, vary=False)
+params.add('n_rays', value=1*10**7, vary=False)
 
 # importing data to fit to
 data = np.genfromtxt('data/run138_minus_run137_data.csv')
@@ -55,7 +57,7 @@ results = np.array(results)
 # saving results
 np.savetxt(f'fits/fit_results_{fit_number}.csv', results)
 
-with open(f'fits/fit_results_{fit_number}.csv') as file:
-    file.write("\n {params['repeat_sim']} \n {params['n_rays']} \n")
+with open(f'fits/fit_results_{fit_number}.csv', 'a') as file:
+    file.write(f"\n {params['repeat_sim']} \n {params['n_rays']} \n")
 
 print('Fitting done!')
